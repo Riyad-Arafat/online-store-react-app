46772676c3d55456dd3e20dbd02084ad336ced64
Riyad <riyad.elberkawy1@gmail.com>
2020-09-14 17:54:43 +0200

    handle hashHistory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router ,Route, Switch} from 'react-router-dom';
 import './App.css';
 
 /// Main Components
@@ -13,12 +13,14 @@ import CartPage from './Pages/CartPage';
 import ProductDetails from './Pages/ProductDetails';
 import Section from './Pages/Section';
 import SearchPage from './Pages/Search';
+import {createHashHistory} from 'history'
 
+const h = createHashHistory(); 
 
 class App extends Component {
   render(){
     return (
-      <BrowserRouter>
+      <Router history={h}>
         <div className="App">
           <NavBar />
           <SideNav />
@@ -32,7 +34,7 @@ class App extends Component {
           </Switch>
           <Footer />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
